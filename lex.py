6c20e967e3fdd2428c88286ef89a6eea7cba1e60
EvaldoFilho098 <47992221+EvaldoFilho098@users.noreply.github.com>
2021-10-27 17:05:41 -0400

    Add files via upload

--- lex.py
+++ lex.py
@@ -37,24 +37,12 @@ tokens = (
     'fcol',
     'acha',
     'fcha',
-    'pcom',
-    'fcon',
-    'pas',
-    'esp',
-    'tab',
-    'ttab',
-    'lin',
-    'alf',
-    'exc',
-    'car',
-    'bug',
-    'erro'
  )
  
 t_digito = r'[0-9]'
 t_letra = r'[a-zA-Z]'
-t_numero = r'{t_digito}+'
-t_ID = r'{t_letra}+'
+t_numero = r'{digito}+'
+t_ID = r'{letra}+'
 
 t_IF = r'if'
 t_ELSE = r'else'
@@ -74,47 +62,17 @@ t_menig = r'<='
 t_maiig = r'>='
 t_ig = r'=='
 t_dif = r'!='
-t_pv = r';'
 t_vir = r','
+t_pv = r';'
 t_apar = r'\('
 t_fpar = r'\)'
 t_acol = r'\['
 t_fcol = r'\]'
 t_acha = r'\{'
 t_fcha = r'\}'
-t_pcom = r'/\*'
-t_fcon = r'\*/'
-t_pas = r'\\0'
-#t_esp = r' '
-#t_tab = None
-#t_ttab = None
-#t_lin = r'\\n'
-#t_alf = None
-#t_exc = None
-#t_car = None
-#t_bug = None
-#t_erro = None
-#reserved = {
-#    'if' : 'IF',
-#    'else' : 'ELSE',
-#    'int' : 'INT',
-#    'void' : 'VOID',
-#    'return' : 'RETURN',
-#    'while' : 'WHILE',
-# }
 # Regular expression rules for simple tokens
-#t_PLUS    = r'\+'
-#t_MINUS   = r'-'
-#t_TIMES   = r'\*'
-#t_DIVIDE  = r'/'
-#t_LPAREN  = r'\('
-#t_RPAREN  = r'\)'
  
- # A regular expression rule with some action code
-#def t_NUMBER(t):
-    #r'\d+'
-    #t.value = int(t.value)    
-    #return t
+# A regular expression rule with some action code
 
 # Define a rule so we can track line numbers
 def t_newline(t):
--- yacc.py
+++ yacc.py
@@ -7,75 +7,75 @@ from lex import tokens
 
 def p_programa(p):
     'programa : lista_declaracao'
-    p[0] = p[1]
+    #p[0] = p[1]
 
 def p_lista_declaracao(p):
     """
-    lista_declaracoes: lista_declaracao declaracao
+    lista_declaracao : lista_declaracao declaracao
                      | declaracao 
     """
 
 def p_declaracao(p):
     """
-    declaracao: declaracao_variaveis
+    declaracao : declaracao_variaveis
               | declaracao_funcoes
     """
 
 def p_declaracao_variaveis(p):
     """
-    declaracao_variaveis: tipo ID ;
-                        | tipo ID[ NUM ];
+    declaracao_variaveis : tipo ID pv
+                        | tipo ID acol numero fcol pv
     """
 
 def p_tipo(p):
     """
-    tipo: int
-        | void
+    tipo : INT
+        | VOID
     """
 
 def p_declaracao_funcoes(p):
     """
-    declaracao_funcoes: tipo ID ( parametros ) declaracao_composta
+    declaracao_funcoes : tipo ID apar parametros fpar declaracao_composta
     """
 
 def p_parametros(p):
     """
-    parametros: lista_parametros
-              | void
+    parametros : lista_parametros
+              | VOID
     """
 
 def p_lista_parametros(p):
     """
-    lista_parametros: lista_parametros , param 
+    lista_parametros : lista_parametros vir param 
                     | param 
     """
 
 def p_param(p):
     """
-    param: tipo ID
-         | tipo ID []
+    param : tipo ID
+         | tipo ID acol fcol
     """
 
 def p_declaracao_composta(p):
     """
-    declaracao_composta: { declaracoes_locais lista comandos }
+    declaracao_composta : acha declaracoes_locais lista_comandos fcha
     """
 
 def p_declaracoes_locais(p):
     """
-    declaracoes_locais: declaracoes_locais declaracao_variaveis
+    declaracoes_locais : declaracoes_locais declaracao_variaveis
                       | 
     """
 
 def p_lista_comandos(p):
     """
-    lista_comandos: lista_comandos comando
-                  | 
+    lista_comandos : lista_comandos comando
+                  | empty
     """
 
 def p_comando(p):
     """
-    comando: declaracao_expressao
+    comando : declaracao_expressao
            | declaracao_composta
            | declaracao_selecao
            | declaracao_iteracao
@@ -84,135 +84,109 @@ def p_comando(p):
 
 def p_declaracao_expressao(p):
     """
-    declaracao_expressao: expressao ;
-             | ;
+    declaracao_expressao : expressao pv
+             | pv
     """
 
 def p_declaracao_selecao(p):
     """
-    declaracao_selecao: if ( expressao ) comando
-                      | if ( expressao ) comando else comando
+    declaracao_selecao : IF apar expressao fpar comando
+                      | IF apar expressao fpar comando ELSE comando
     """
 
 def p_declaracao_iteracao(p):
     """
-    declaracao_iteracao: while ( expressao ) comando
+    declaracao_iteracao : WHILE apar expressao fpar comando
     """
 
 def p_declaracao_retorno(p):
     """
-    declaracao_retorno: return ;
-                      | return expressao ;
+    declaracao_retorno : RETURN pv
+                      | RETURN expressao pv
     """
 
 def p_expressao(p):
     """
-    expressao: variavel = expressao
+    expressao : variavel atr expressao
              | expressao_simples
     """
 
 def p_variavel(p):
     """
-    variavel: ID
-            | ID [ expressao ]
+    variavel : ID
+            | ID acol expressao fcol
     """
 
 def p_expressao_simples(p):
     """
-    expressao_simples: soma_expressao op_relacional soma_expressao
+    expressao_simples : soma_expressao op_relacional soma_expressao
                      | soma_expressao
     """
 
 def p_op_relacional(p):
     """
-    op_relacional: <=
-                 | <
-                 | >
-                 | >=
-                 | ==
-                 | !=
+    op_relacional : menig
+                 | men
+                 | mai
+                 | maiig
+                 | ig
+                 | dif
     """
 
 def p_soma_expressao(p):
     """
-    soma_expressao: soma_expressao soma termo
+    soma_expressao : soma_expressao soma termo
                   | termo
     """
 
 def p_soma(p):
     """
-    soma: +
-        | -
+    soma : add
+        | sub
     """
 
 def p_termo(p):
     """
-    termo: termo mult fator
+    termo : termo mult fator
          | fator
     """
 
 def p_mult(p):
     """
-    mult: *
-        | /
+    mult : mlt
+        | divv
     """
 
 def p_fator(p):
     """
-    fator: ( expressao )
+    fator : apar expressao fpar
          | variavel
          | ativacao
-         | NUM
+         | numero
     """
 
 def p_ativacao(p):
     """
-    ativacao: ID ( argumentos )
+    ativacao : ID apar argumentos fpar
     """
 
 def p_argumentos(p):
     """
-    argumentos: lista_argumentos
-              | 
+    argumentos : lista_argumentos
+              | empty
     """
 
 def p_lista_argumentos(p):
     """
-    lista_argumentos: lista_argumentos , expressao
+    lista_argumentos : lista_argumentos vir expressao
                     | expressao
     """
-    
-def p_expression_plus(p):
-    'expression : expression PLUS term'
-    p[0] = p[1] + p[3]
 
-def p_expression_minus(p):
-    'expression : expression MINUS term'
-    p[0] = p[1] - p[3]
-
-def p_expression_term(p):
-    'expression : term'
-    p[0] = p[1]
-
-def p_term_times(p):
-    'term : term TIMES factor'
-    p[0] = p[1] * p[3]
-
-def p_term_div(p):
-    'term : term DIVIDE factor'
-    p[0] = p[1] / p[3]
-
-def p_term_factor(p):
-    'term : factor'
-    p[0] = p[1]
-
-def p_factor_num(p):
-    'factor : NUMBER'
-    p[0] = p[1]
-
-def p_factor_expr(p):
-    'factor : LPAREN expression RPAREN'
-    p[0] = p[2]
+def p_empty(p):
+     """
+     empty :
+     """
+     pass 
 
 # Error rule for syntax errors
 def p_error(p):
@@ -222,10 +196,6 @@ def p_error(p):
 parser = yacc.yacc()
 
 while True:
-    try:
-        s = input('calc > ')
-    except EOFError:
-        break
-    if not s: continue
+    s = input('calc > ')
     result = parser.parse(s)
     print(result)
